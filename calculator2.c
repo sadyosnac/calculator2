e06cd0c3c17445a56b6fa4db14b400a830261b3e
sadyosnac <can2322@hotmail.com>
2020-01-02 16:11:14 +0300

    initial commit

---
+++ calculator2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a1;
+	int a2;
+	printf("Lutfen bir sayi giriniz:");
+	scanf("%d",&a1);
+	printf("Lutfen ikinci bir sayi giriniz:");
+	scanf("%d",&a2);
+	
+	printf("Sonuc: %d",a1+a2);
+}
